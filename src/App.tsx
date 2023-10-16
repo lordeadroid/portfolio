e0f44777476fdf72ce1dbd4fb31915ca7c867d6c
lordeadroid <rishabhrawat34@gmail.com>
2023-10-16 18:54:07 +0530

    Feature: Added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+import Navbar from './navbar';
+
 const App = () => {
   return (
     <div>
+      <Navbar />
       <h1 className="name">Rishabh Rawat</h1>
     </div>
   );
---
+++ src/navbar.tsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div>First</div>
+      <div>Second</div>
+      <div>Third</div>
+    </nav>
+  );
+};
+
+export default Navbar;
